2784408d2050148e55afcbbbfa79747f327b485a
Federico De Sía <desiafederico@gmail.com>
2021-11-29 16:17:44 -0300

    CategoryScreen appbar

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -6,12 +6,18 @@ class RoundedButton extends StatelessWidget{
   final Widget child;
   final Color color;
   final double width;
+  final double height;
+  final double borderRadius;
+  final EdgeInsets padding;
   final Function() onPressed;
 
   const RoundedButton({
     required this.child,
     this.color = cPrimaryColor,
-    required this.width,
+    this.width = cButtonSize,
+    this.height = cButtonSize,
+    this.borderRadius = cBorderRadius,
+    this.padding = const EdgeInsets.all(cButtonPadding),
     required this.onPressed
   });
   
@@ -19,15 +25,15 @@ class RoundedButton extends StatelessWidget{
   Widget build(BuildContext context) {
     return Container(
       width: width,
-      height: cButtonSize,
+      height: height,
       child: ElevatedButton(
         onPressed: onPressed,
         child: child,
         style: ElevatedButton.styleFrom(
           primary: color,
-          padding: EdgeInsets.all(cButtonPadding),
+          padding: padding,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(cBorderRadius),
+            borderRadius: BorderRadius.circular(borderRadius),
           ),
         ),
       ),
--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_manager/blocs/category_bloc/category_bloc.dart';
+import 'package:task_manager/components/responsive/widget_size.dart';
+import 'package:task_manager/components/rounded_button.dart';
 import 'package:task_manager/models/category.dart';
 import '../constants.dart';
 
@@ -13,7 +15,9 @@ class CategoryScreen extends StatefulWidget{
   _CategoryScreenState createState() => _CategoryScreenState();
 }
 
-class _CategoryScreenState extends State<CategoryScreen> with TickerProviderStateMixin{
+class _CategoryScreenState extends State<CategoryScreen>{
+
+  double appBarHeight = 500.0;
 
   @override
   Widget build(BuildContext context){
@@ -26,53 +30,126 @@ class _CategoryScreenState extends State<CategoryScreen> with TickerProviderStat
           
           Category category = (categoryState as CategoryLoadSuccess).categories.firstWhere((c) => c.uuid == widget.categoryUuid);
 
-          return CustomScrollView(
-            physics: BouncingScrollPhysics(
-              parent: AlwaysScrollableScrollPhysics()
-            ),
-            slivers: [
+          return SafeArea(
+            child: CustomScrollView(
+              physics: BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics()
+              ),
+              slivers: [
 
-              SliverAppBar(
-                backgroundColor: cCardBackgroundColor,
+                SliverAppBar(
+                  backgroundColor: Colors.transparent,
 
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(cBorderRadius),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(cBorderRadius),
+                    ),
+                  ),
+
+                  automaticallyImplyLeading: false,
+                  collapsedHeight: appBarHeight,
+                  flexibleSpace: WidgetSize(
+                    onChange: (Size size) => setState(() => appBarHeight = size.height),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(
+                                color: Colors.white.withOpacity(0.75),
+                                icon: Icon(Icons.navigate_before_rounded),
+                                splashRadius: 32.0,
+                                onPressed: () {
+                                  Navigator.of(context).maybePop();
+                                },
+                              ),
+
+                              Row(
+                                children: [
+                                  Container(
+                                    height: 8.0,
+                                    width: 8.0,
+                                    decoration: BoxDecoration(
+                                      color: category.color,
+                                      borderRadius: BorderRadius.all(Radius.circular(8.0))
+                                    )
+                                  ),
+                                  SizedBox(width: 8.0),
+                                  Text(
+                                    category.name,
+                                    style: cTitleTextStyle.copyWith(fontSize: 16.0, height: 1.0),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  )
+                                ],
+                              ),
+
+                              IconButton(
+                                color: Colors.white.withOpacity(0.75),
+                                icon: Icon(Icons.more_vert_rounded),
+                                splashRadius: 32.0,
+                                onPressed: () {},
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: cPadding - 16.0),
+                        
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: cPadding),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(Radius.circular(cBorderRadius)),
+                                    color: cCardBackgroundColor
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(16.0),
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.search_rounded,
+                                          color: Colors.white.withOpacity(0.25),
+                                        ),
+                                        SizedBox(width: 12.0),
+                                        Text(
+                                          "Search task...",
+                                          style: cLightTextStyle.copyWith(height: 1.0),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                              SizedBox(width: 12.0),
+                              RoundedButton(
+                                width: 56.0,
+                                height: 56.0,
+                                color: cCardBackgroundColor,
+                                child: Icon(
+                                  Icons.tune_rounded,
+                                  color: Colors.white.withOpacity(0.75)
+                                ),
+                                onPressed: () {},
+                              )
+                            ],
+                          ),
+                        ),
+
+                        SizedBox(height: cPadding),
+                      ],
+                    ),
                   ),
                 ),
-
-                centerTitle: true,
-                title: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 8.0,
-                      width: 8.0,
-                      decoration: BoxDecoration(
-                        color: category.color,
-                        borderRadius: BorderRadius.all(Radius.circular(8.0))
-                      )
-                    ),
-                    SizedBox(width: 8.0),
-                    Text(
-                      category.name,
-                      style: cTitleTextStyle.copyWith(fontSize: 17.0, height: 1.0),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    )
-                  ],
-                ),
-
-                actions: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.more_vert_rounded)
-                  )
-                ],
-              ),
-            ]
+              ]
+            ),
           );
         }
       )
