3b4e9f7dc6d57c2c999eb5a4d00a540dffa562ed
Federico De Sía <desiafederico@gmail.com>
2021-12-01 01:18:55 -0300

    Temp category uuid hardcoded

--- lib/repositories/category_repository.dart
+++ lib/repositories/category_repository.dart
@@ -10,8 +10,8 @@ class CategoryRepository{
     await Future.delayed(Duration(milliseconds: 500));
     final List<Category> categories = [];
 
-    categories.add(Category(uuid: Uuid().v4(), name: "School", color: Color(0xFFFF6B55)));
-    categories.add(Category(uuid: Uuid().v4(), name: "Personal", color: Color(0xFF07B9AE)));
+    categories.add(Category(uuid: "b2d60aa1-27b7-45c3-8f92-39f66ec7ed27", name: "School", color: Color(0xFFFF6B55)));
+    categories.add(Category(uuid: "520b1787-46e5-4aa4-84fe-4a7d267b84a7", name: "Personal", color: Color(0xFF07B9AE)));
     categories.add(Category(uuid: Uuid().v4(), name: "Design", color: Color(0xFF7F43FF)));
     categories.add(Category(uuid: Uuid().v4(), name: "Groceries", color: Color(0xFFDDBC10)));
 
--- lib/repositories/task_repository.dart
+++ lib/repositories/task_repository.dart
@@ -11,9 +11,9 @@ class TaskRepository{
     await Future.delayed(Duration(milliseconds: 500));
     final List<Task> taskList = [];
 
-    taskList.add(Task(uuid: Uuid().v4(), title: "Wake up buddy")..setDateTime(copyDateTimeWith(DateTime.now(), hour: 7)));
-    taskList.add(Task(uuid: Uuid().v4(), title: "Daily workout")..setDateTime(copyDateTimeWith(DateTime.now(), hour: 7)));
-    taskList.add(Task(uuid: Uuid().v4(), title: "Shrink project kick off", description: "Skype call, kick off with Elena and Andrew from Shrink")..setDateTime(copyDateTimeWith(DateTime.now(), hour: 10)));
+    taskList.add(Task(uuid: Uuid().v4(), title: "Wake up buddy", categoryUuid: "520b1787-46e5-4aa4-84fe-4a7d267b84a7", completed: true)..setDateTime(copyDateTimeWith(DateTime.now(), hour: 7)));
+    taskList.add(Task(uuid: Uuid().v4(), title: "Daily workout", categoryUuid: "520b1787-46e5-4aa4-84fe-4a7d267b84a7")..setDateTime(copyDateTimeWith(DateTime.now(), hour: 7)));
+    taskList.add(Task(uuid: Uuid().v4(), title: "Shrink project kick off", description: "Skype call, kick off with Elena and Andrew from Shrink", categoryUuid: "b2d60aa1-27b7-45c3-8f92-39f66ec7ed27")..setDateTime(copyDateTimeWith(DateTime.now(), hour: 10)));
     taskList.add(Task(uuid: Uuid().v4(), title: "Hangouts Sushi", description: "Lauch with Julia, fight this quarantine with humor")..setDateTime(copyDateTimeWith(DateTime.now(), hour: 12)));
     
     final List<Task> taskListTwo = [];
@@ -34,6 +34,7 @@ class TaskRepository{
           uuid: Uuid().v4(),
           title: newTaskTitle,
           description: task.description,
+          completed: false
         )..setDateTime(task.dateTime.add(Duration(days: i + 1))));
       }
     }
