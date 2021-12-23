130098b27a5934413011c60181fcf818577e523e
Federico De Sía <desiafederico@gmail.com>
2021-12-23 16:21:17 -0300

    LoginScreen Form validation and scroll fixes.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,31 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_manager/components/forms/rounded_text_form_field.dart';
-import 'package:task_manager/components/responsive/fill_remaining_list.dart';
-import 'package:task_manager/components/responsive/widget_size.dart';
 import 'package:task_manager/components/rounded_button.dart';
-import 'package:task_manager/cubits/available_space_cubit.dart';
 import '../constants.dart';
 
-class LoginScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => AvailableSpaceCubit(),
-      child: _LoginScreen(),
-    );
-  }
-}
-
-class _LoginScreen extends StatefulWidget{
+class LoginScreen extends StatefulWidget{
 
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<_LoginScreen>{
-
-  late AvailableSpaceCubit availableSpaceCubit = BlocProvider.of<AvailableSpaceCubit>(context);
+class _LoginScreenState extends State<LoginScreen>{
+  
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   bool obscurePassword = true;
 
   @override
@@ -35,131 +21,146 @@ class _LoginScreenState extends State<_LoginScreen>{
       backgroundColor: cBackgroundColor,
       body: SafeArea(
         child: LayoutBuilder(
-          builder: (_, constraints) {
+          builder: (context, constraints){
 
             return SingleChildScrollView(
               physics: BouncingScrollPhysics(
                 parent: AlwaysScrollableScrollPhysics()
               ),
-              child: Column(
-                children: [
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minWidth: constraints.maxWidth, minHeight: constraints.maxHeight),
+                child: IntrinsicHeight(
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
 
-                  WidgetSize(
-                    onChange: (Size size){
-                      context.read<AvailableSpaceCubit>().setHeight(constraints.maxHeight - size.height);
-                    },
-                    child: Padding(
-                      padding: EdgeInsets.all(cPadding),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-
-                          SizedBox(height: cPadding),
-                          Text(
-                            "Welcome back!",
-                            style: cTitleTextStyle,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          SizedBox(height: 8.0),
-
-                          Text(
-                            "Please sign in to your account",
-                            style: cSmallLightTextStyle,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          SizedBox(height: cPadding * 2),
-
-                          RoundedTextFormField(
-                            hintText: "Email",
-                          ),
-                          SizedBox(height: 16.0),
-
-                          RoundedTextFormField(
-                            hintText: "Password",
-                            obscureText: obscurePassword,
-                            suffixIcon: Padding(
-                              padding: EdgeInsets.only(right: 8.0),
-                              child: Material(
-                                color: Colors.transparent,
-                                child: IconButton(
-                                  icon: AnimatedSwitcher(
-                                    duration: cFastAnimationDuration,
-                                    child: Icon(
-                                      obscurePassword ? Icons.visibility_outlined : Icons.visibility_off_outlined,
-                                      key: Key("IconButtonObscurePasswordKeyValue=$obscurePassword"),
-                                    ),
-                                  ),
-                                  splashRadius: 24.0,
-                                  color: cLightGrayColor,
-                                  onPressed: () {
-                                    setState(() => obscurePassword = !obscurePassword);
-                                  },
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 16.0),
-
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
+                        Padding(
+                          padding: EdgeInsets.all(cPadding),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
                             children: [
+
+                              SizedBox(height: cPadding),
                               Text(
-                                "Forgot Password?",
-                                style: cSmallLightTextStyle,
-                                maxLines: 1,
+                                "Welcome back!",
+                                style: cTitleTextStyle,
+                                maxLines: 2,
                                 overflow: TextOverflow.ellipsis,
                               ),
+                              SizedBox(height: 8.0),
+
+                              Text(
+                                "Please sign in to your account",
+                                style: cSmallLightTextStyle,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              SizedBox(height: cPadding * 2),
+
+                              RoundedTextFormField(
+                                hintText: "Email",
+                                validator: (value){
+                                  value = value ?? "";
+                                  if(value.isEmpty) return "Please enter your email";
+                                  return null;
+                                }
+                              ),
+                              SizedBox(height: 16.0),
+
+                              RoundedTextFormField(
+                                hintText: "Password",
+                                obscureText: obscurePassword,
+                                suffixIcon: Padding(
+                                  padding: EdgeInsets.only(right: 8.0),
+                                  child: Material(
+                                    color: Colors.transparent,
+                                    child: IconButton(
+                                      icon: AnimatedSwitcher(
+                                        duration: cFastAnimationDuration,
+                                        child: Icon(
+                                          obscurePassword ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+                                          key: Key("IconButtonObscurePasswordKeyValue=$obscurePassword"),
+                                        ),
+                                      ),
+                                      splashRadius: 24.0,
+                                      color: cLightGrayColor,
+                                      onPressed: () {
+                                        setState(() => obscurePassword = !obscurePassword);
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                validator: (value){
+                                  value = value ?? "";
+                                  if(value.isEmpty) return "Please enter your password";
+                                  return null;
+                                }
+                              ),
+                              SizedBox(height: 16.0),
+
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "Forgot Password?",
+                                    style: cSmallLightTextStyle,
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ],
+                              ),                          
                             ],
-                          ),                          
-                        ],
-                      ),
+                          ),
+                        ),
+                        
+                        Spacer(),
+
+                        Padding(
+                          padding: EdgeInsets.all(cPadding),
+                          child: Column(
+                            children: [
+                              RoundedButton(
+                                color: cCardBackgroundColor,
+                                width: double.infinity,
+                                child: Text(
+                                  "Sign In",
+                                  style: cBoldTextStyle,
+                                ),
+                                onPressed: () {
+                                  if(formKey.currentState!.validate()){
+                                    formKey.currentState!.save();
+                                  }
+                                },
+                              ),
+                              SizedBox(height: cPadding),
+
+                              RichText(
+                                text: TextSpan(
+                                  style: cSmallLightTextStyle,
+                                  children: <TextSpan>[
+                                    TextSpan(text: "Don't have an Account? "),
+                                    TextSpan(text: "Sign Up", style: cSmallLightTextStyle.copyWith(color: cTextButtonColor)),
+                                    TextSpan(text: "\n")
+                                  ],
+                                ),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                        )
+                      ]
                     ),
                   ),
-
-                  FillRemainingList(
-                    availableSpaceCubit: availableSpaceCubit,
-                    alignment: Alignment.bottomCenter,
-                    subtractPadding: false,
-                    child: Padding(
-                      padding: EdgeInsets.all(cPadding),
-                      child: Column(
-                        children: [
-                          RoundedButton(
-                            color: cCardBackgroundColor,
-                            width: double.infinity,
-                            child: Text(
-                              "Sign In",
-                              style: cBoldTextStyle,
-                            ),
-                            onPressed: () {},
-                          ),
-                          SizedBox(height: cPadding),
-
-                          RichText(
-                            text: TextSpan(
-                              style: cSmallLightTextStyle,
-                              children: <TextSpan>[
-                                TextSpan(text: "Don't have an Account? "),
-                                TextSpan(text: "Sign Up", style: cSmallLightTextStyle.copyWith(color: cTextButtonColor)),
-                              ],
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: cPadding),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+                )
               )
             );
           }
         ),
-      ),
+      )
     );
   }
 }
