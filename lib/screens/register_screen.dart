0857f54448de1c9b41b98411ed40674be826065d
Federico De Sía <desiafederico@gmail.com>
2021-12-27 01:31:45 -0300

    Start of RegisterScreen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/components/forms/rounded_text_form_field.dart';
 import 'package:task_manager/components/rounded_button.dart';
-import 'package:task_manager/screens/register_screen.dart';
+import 'package:task_manager/screens/login_screen.dart';
+import 'package:task_manager/screens/welcome_screen.dart';
 import '../constants.dart';
 
-class LoginScreen extends StatefulWidget{
+class RegisterScreen extends StatefulWidget{
 
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _RegisterScreenState createState() => _RegisterScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen>{
+class _RegisterScreenState extends State<RegisterScreen>{
   
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
   bool obscurePassword = true;
@@ -45,7 +46,7 @@ class _LoginScreenState extends State<LoginScreen>{
 
                               SizedBox(height: cPadding),
                               Text(
-                                "Welcome back!",
+                                "Create new account",
                                 style: cTitleTextStyle,
                                 maxLines: 2,
                                 overflow: TextOverflow.ellipsis,
@@ -53,12 +54,22 @@ class _LoginScreenState extends State<LoginScreen>{
                               SizedBox(height: 8.0),
 
                               Text(
-                                "Please sign in to your account",
+                                "Please fill in the form to continue",
                                 style: cLightTextStyle,
                                 maxLines: 2,
                                 overflow: TextOverflow.ellipsis,
                               ),
                               SizedBox(height: cPadding * 2),
+
+                              RoundedTextFormField(
+                                hintText: "Name",
+                                validator: (value){
+                                  value = value ?? "";
+                                  if(value.isEmpty) return "Please enter your name";
+                                  return null;
+                                }
+                              ),
+                              SizedBox(height: 16.0),
 
                               RoundedTextFormField(
                                 hintText: "Email",
@@ -98,20 +109,7 @@ class _LoginScreenState extends State<LoginScreen>{
                                   if(value.isEmpty) return "Please enter your password";
                                   return null;
                                 }
-                              ),
-                              SizedBox(height: 16.0),
-
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    "Forgot Password?",
-                                    style: cSmallLightTextStyle,
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ],
-                              ),                          
+                              ),                   
                             ],
                           ),
                         ),
@@ -126,7 +124,7 @@ class _LoginScreenState extends State<LoginScreen>{
                                 color: cCardBackgroundColor,
                                 width: double.infinity,
                                 child: Text(
-                                  "Sign In",
+                                  "Sign Up",
                                   style: cBoldTextStyle,
                                 ),
                                 onPressed: () {
@@ -142,8 +140,8 @@ class _LoginScreenState extends State<LoginScreen>{
                                   text: TextSpan(
                                     style: cSmallLightTextStyle,
                                     children: <TextSpan>[
-                                      TextSpan(text: "Don't have an Account? "),
-                                      TextSpan(text: "Sign Up", style: cSmallLightTextStyle.copyWith(color: cTextButtonColor)),
+                                      TextSpan(text: "Already have an Account? "),
+                                      TextSpan(text: "Sign In", style: cSmallLightTextStyle.copyWith(color: cTextButtonColor)),
                                       TextSpan(text: "\n")
                                     ],
                                   ),
@@ -152,9 +150,7 @@ class _LoginScreenState extends State<LoginScreen>{
                                   textAlign: TextAlign.center,
                                 ),
                                 onTap: () {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(builder: (context) => RegisterScreen())
-                                  );
+                                  Navigator.of(context).pop();
                                 },
                               ),
                             ],
